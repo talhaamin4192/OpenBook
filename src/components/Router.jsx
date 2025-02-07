68ceb786e39d3632fcce709f546c065f5c330517
Muhamamd Talha Amin <154052586+talhaamin4192@users.noreply.github.com>
2025-02-07 16:39:07 +0500

    Initial commit

---
+++ src/components/Router.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Home from "./Home"
+const Router = () => {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home />
+    }
+    ,
+  ]
+  )
+  return (
+    <>
+      <RouterProvider router={router}></RouterProvider>
+    </>
+  )
+}
+
+export default Router
