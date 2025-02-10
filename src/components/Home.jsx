8fe5556bb6676e171f0a85df48b1270b66a876a7
Muhamamd Talha Amin <154052586+talhaamin4192@users.noreply.github.com>
2025-02-10 15:03:54 +0500

    Setting Up

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,8 +3,11 @@ import React from 'react'
 const Home = () => {
   return (
     <>
+    <div className='font-display'>
+      Uncovering Hidden Meaning A Guide
+    </div>
     <div>
-        
+    Uncovering Hidden Meaning A Guide
     </div>
     </>
   )
