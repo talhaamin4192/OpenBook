62874d6426940885707bf12d4c56b83e42ad6934
Muhamamd Talha Amin <154052586+talhaamin4192@users.noreply.github.com>
2025-02-14 15:17:22 +0500

    added an svg

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,14 +1,28 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
+import logo from '../assets/logo.svg'
 const Home = () => {
   return (
     <>
-    <div className='font-display'>
-      Uncovering Hidden Meaning A Guide
-    </div>
-    <div>
-    Uncovering Hidden Meaning A Guide
-    </div>
+      <div className=' h-20 flex justify-between px-8  items-center font-semibold'>
+        <div className='flex '>
+          <img src={logo} alt="" />
+          <h2>OpenBook</h2>
+        </div>
+        <div className='flex  justify-end gap-40 ' >
+          <div className='[&>*]:block [&>*]:hover:underline' >
+            <Link>Symbols</Link>
+            <Link>Types</Link>
+          </div>
+          <div className='[&>*]:block [&>*]:hover:underline'>
+            <Link>Oracle AI</Link>
+            <Link>About</Link>
+          </div>
+          <div>
+            <Link className='hover:underline'>Contribute</Link>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
