94d1ffa4589c5e05cbfbdef079ef857f9f259a80
Muhamamd Talha Amin <154052586+talhaamin4192@users.noreply.github.com>
2025-02-16 00:37:16 +0500

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,25 +4,35 @@ import logo from '../assets/logo.svg'
 const Home = () => {
   return (
     <>
-      <div className=' h-20 flex justify-between px-8  items-center font-semibold'>
-        <div className='flex '>
-          <img src={logo} alt="" />
-          <h2>OpenBook</h2>
+      <div>
+        <div className=' h-20 flex justify-between px-8  items-center font-semibold'>
+          <div className='flex '>
+            <img src={logo} alt="" />
+            <h2>OpenBook</h2>
+          </div>
+          <div className='flex  justify-end gap-40 ' >
+            <div className='[&>*]:block [&>*]:hover:underline' >
+              <Link>Symbols</Link>
+              <Link>Types</Link>
+            </div>
+            <div className='[&>*]:block [&>*]:hover:underline'>
+              <Link>Oracle AI</Link>
+              <Link>About</Link>
+            </div>
+            <div>
+              <Link className='hover:underline'>Contribute</Link>
+            </div>
+          </div>
         </div>
-        <div className='flex  justify-end gap-40 ' >
-          <div className='[&>*]:block [&>*]:hover:underline' >
-            <Link>Symbols</Link>
-            <Link>Types</Link>
-          </div>
-          <div className='[&>*]:block [&>*]:hover:underline'>
-            <Link>Oracle AI</Link>
-            <Link>About</Link>
-          </div>
-          <div>
-            <Link className='hover:underline'>Contribute</Link>
-          </div>
+        <div>
+          <div>Uncovering</div>
+          <div> Hidden Meaning -</div>
+          <div>A Guide to Dream</div>
+          <div>Analysis and Symbols</div>
+          <div>in the Modern World.</div>
         </div>
       </div>
+
     </>
   )
 }
